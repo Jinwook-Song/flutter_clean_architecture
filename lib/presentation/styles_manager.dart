f3d6f39dd3c087321d164d87315859f3c298e09a
jinwook song <wlsdnr129@naver.com>
2024-09-21 22:10:44 +0900

    presentation(resources): styles manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,37 @@
+import 'package:clean_architecture/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+TextStyle _getTextStyle(double fontSize, Color color, FontWeight fontWeight,
+    [String? fontFamily = FontConstants.fontFamily]) {
+  return TextStyle(
+    fontSize: fontSize,
+    color: color,
+    fontWeight: fontWeight,
+    fontFamily: fontFamily,
+  );
+}
+
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, color, FontWeightManager.regular);
+}
+
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, color, FontWeightManager.light);
+}
+
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, color, FontWeightManager.bold);
+}
+
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, color, FontWeightManager.semiBold);
+}
+
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, color, FontWeightManager.medium);
+}
