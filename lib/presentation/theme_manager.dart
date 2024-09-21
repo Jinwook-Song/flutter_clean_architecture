24fbb4418da1c17531b8fb3fd4f309f26066197b
jinwook song <wlsdnr129@naver.com>
2024-09-21 21:51:27 +0900

    presentation(resources): theme manager, card theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,19 +1,25 @@
 import 'package:clean_architecture/presentation/color_manager.dart';
+import 'package:clean_architecture/presentation/value_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getAppTheme() {
   return ThemeData(
-    // main colors of the app
-    colorScheme: ColorScheme.fromSeed(seedColor: ColorManager.primary),
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
+      // main colors of the app
+      colorScheme: ColorScheme.fromSeed(seedColor: ColorManager.primary),
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
 
-    // card view theme
-    // App bar theme
-    // Button theme
-    // Text theme
-    // input decoration theme (text form field)
-  );
+      // card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      )
+      // App bar theme
+      // Button theme
+      // Text theme
+      // input decoration theme (text form field)
+      );
 }
