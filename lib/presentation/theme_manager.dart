83bb5488d3cc61d793b57ebb1c7adb2126280894
jinwook song <wlsdnr129@naver.com>
2024-09-21 22:12:15 +0900

    presentation(resources): theme manager, app bar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,25 +1,41 @@
 import 'package:clean_architecture/presentation/color_manager.dart';
+import 'package:clean_architecture/presentation/font_manager.dart';
+import 'package:clean_architecture/presentation/styles_manager.dart';
 import 'package:clean_architecture/presentation/value_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getAppTheme() {
   return ThemeData(
-      // main colors of the app
-      colorScheme: ColorScheme.fromSeed(seedColor: ColorManager.primary),
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
+    // main colors of the app
+    colorScheme: ColorScheme.fromSeed(seedColor: ColorManager.primary),
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
 
-      // card view theme
-      cardTheme: CardTheme(
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
+
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+    // App bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
         color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSize.s4,
-      )
-      // App bar theme
-      // Button theme
-      // Text theme
-      // input decoration theme (text form field)
-      );
+        fontSize: FontSize.s16,
+      ),
+    ),
+
+    // Button theme
+    // Text theme
+    // input decoration theme (text form field)
+  );
 }
