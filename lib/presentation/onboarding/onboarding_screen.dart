4dcd40f2d3a4b6ba654a077ae566e1982ee10e03
jinwook song <wlsdnr129@naver.com>
2024-09-25 21:06:49 +0900

    feat(carousel): onboarding

--- lib/presentation/onboarding/onboarding_screen.dart
+++ lib/presentation/onboarding/onboarding_screen.dart
@@ -1,14 +1,209 @@
+import 'package:clean_architecture/presentation/resources/assets_manager.dart';
 import 'package:clean_architecture/presentation/resources/color_manager.dart';
+import 'package:clean_architecture/presentation/resources/strings_manager.dart';
+import 'package:clean_architecture/presentation/resources/value_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late final List<SliderObject> _list = _getSliderData();
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          title: AppStrings.onBoardingTitle1,
+          subtitle: AppStrings.onBoardingTitle1,
+          image: ImageAssets.onboardingLogo1,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle2,
+          subtitle: AppStrings.onBoardingTitle2,
+          image: ImageAssets.onboardingLogo2,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle3,
+          subtitle: AppStrings.onBoardingTitle3,
+          image: ImageAssets.onboardingLogo3,
+        ),
+        SliderObject(
+          title: AppStrings.onBoardingTitle4,
+          subtitle: AppStrings.onBoardingTitle4,
+          image: ImageAssets.onboardingLogo4,
+        ),
+      ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: ColorManager.white,
-      body: Center(child: Text('Welcome to onboarding')),
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        elevation: AppSize.s0,
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        itemBuilder: (context, index) => OnboardingPage(
+          sliderObject: _list[index],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: ColorManager.primary,
+        height: AppSize.s120,
+        child: Column(
+          children: [
+            Container(
+              color: ColorManager.white,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    AppStrings.skip,
+                    style: Theme.of(context).textTheme.titleSmall,
+                    textAlign: TextAlign.right,
+                  ),
+                ),
+              ),
+            ),
+            _getBottomSheetWidget()
+          ],
+        ),
+      ),
     );
   }
+
+  Widget _getBottomSheetWidget() {
+    return Container(
+      color: ColorManager.primary,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // left arrow
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                height: AppSize.s20,
+                width: AppSize.s20,
+                child: SvgPicture.asset(ImageAssets.leftArrowIc),
+              ),
+              onTap: () {
+                // go to previous slide
+                _pageController.animateToPage(
+                  _currentIndex - 1,
+                  duration: const Duration(milliseconds: DurationConstant.d300),
+                  curve: Curves.bounceInOut,
+                );
+              },
+            ),
+          ),
+
+          // circles indicator
+          Row(
+            children: [
+              for (int i = 0; i < _list.length; i++)
+                Padding(
+                  padding: const EdgeInsets.all(AppPadding.p8),
+                  child: _getProperCircle(i),
+                )
+            ],
+          ),
+
+          // right arrow
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                height: AppSize.s20,
+                width: AppSize.s20,
+                child: SvgPicture.asset(ImageAssets.rightarrowIc),
+              ),
+              onTap: () {
+                // go to next slide
+                _pageController.animateToPage(
+                  _currentIndex + 1,
+                  duration: const Duration(milliseconds: DurationConstant.d300),
+                  curve: Curves.bounceInOut,
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _getProperCircle(int index) {
+    if (index == _currentIndex) {
+      return SvgPicture.asset(ImageAssets.solidCircleIc);
+    } else {
+      return SvgPicture.asset(ImageAssets.hollowCircleIc);
+    }
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  final SliderObject sliderObject;
+  const OnboardingPage({super.key, required this.sliderObject});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Gap(AppSize.s40),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            sliderObject.subtitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+        const Gap(AppSize.s40),
+        SvgPicture.asset(sliderObject.image)
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  final String title;
+  final String subtitle;
+  final String image;
+
+  SliderObject({
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
 }
--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,5 +1,20 @@
 const String imgPath = 'assets/images';
 
 class ImageAssets {
-  static const String splashLogo = '$imgPath/splash_logo.png';
+  static const String splashLogo = "$imgPath/splash_logo.png";
+  static const String onboardingLogo1 = "$imgPath/onboarding_logo1.svg";
+  static const String onboardingLogo2 = "$imgPath/onboarding_logo2.svg";
+  static const String onboardingLogo3 = "$imgPath/onboarding_logo3.svg";
+  static const String onboardingLogo4 = "$imgPath/onboarding_logo4.svg";
+  static const String hollowCircleIc = "$imgPath/hollow_cirlce_ic.svg";
+  static const String leftArrowIc = "$imgPath/left_arrow_ic.svg";
+  static const String rightarrowIc = "$imgPath/right_arrow_ic.svg";
+  static const String solidCircleIc = "$imgPath/solid_circle_ic.svg";
+  static const String photoCameraIc = "$imgPath/photo_camera_ic.svg";
+  static const String settingsRightArrowIc =
+      "$imgPath/settings_right_arrow_ic.svg";
+  static const String changeLangIc = "$imgPath/change_lang_ic.svg";
+  static const String contactUsIc = "$imgPath/contact_us_ic.svg";
+  static const String inviteFriendsIc = "$imgPath/invite_friends_ic.svg";
+  static const String logoutIc = "$imgPath/logout_ic.svg";
 }
