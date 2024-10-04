79449a13a68166881b49707857c81eb00c717b86
jinwook song <wlsdnr129@naver.com>
2024-10-04 16:15:25 +0900

    data(network): dio factory

--- lib/app/constant.dart
+++ lib/app/constant.dart
@@ -1,5 +1,6 @@
 class Constant {
   static const String baseUrl = 'https://jinwook.wiremockapi.cloud/';
+  static const String token = 'get api token here';
   static const empty = '';
   static const zero = 0;
 }
---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:clean_architecture/app/constant.dart';
+import 'package:dio/dio.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+    Duration timeOut = const Duration(minutes: 1);
+
+    Map<String, String> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constant.token,
+      DEFAULT_LANGUAGE: 'en' // default language
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: Constant.baseUrl,
+      connectTimeout: timeOut,
+      receiveTimeout: timeOut,
+      headers: headers,
+    );
+
+    return dio;
+  }
+}
