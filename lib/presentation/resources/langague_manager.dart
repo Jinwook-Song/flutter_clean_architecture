8bda7f2cd08ca4fcc8aa001bf152b19d78aa9d16
jinwook song <wlsdnr129@naver.com>
2024-10-04 17:03:19 +0900

    resources: langague mananger

---
+++ lib/presentation/resources/langague_manager.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+enum LanguageType {
+  ENGLISH(
+    value: 'en',
+    locale: Locale("en", "US"),
+  ),
+  KOREAN(
+    value: 'ko',
+    locale: Locale("ko", "KR"),
+  );
+
+  // 필드 정의
+  final String value;
+  final Locale locale;
+
+  // 생성자 정의
+  const LanguageType({required this.value, required this.locale});
+
+  // 메서드 정의
+  String getValue() => value;
+
+  Locale getLocale() => locale;
+}
