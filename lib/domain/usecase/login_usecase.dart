f0fe13649326d9b8efc7433aefd7cc56b73dce89
jinwook song <wlsdnr129@naver.com>
2024-10-04 17:32:32 +0900

    domain(usecase): login usecase

---
+++ lib/domain/usecase/login_usecase.dart
@@ -0,0 +1,29 @@
+import 'package:clean_architecture/data/network/failure.dart';
+import 'package:clean_architecture/data/request/request.dart';
+import 'package:clean_architecture/domain/model/model.dart';
+import 'package:clean_architecture/domain/repository/repository.dart';
+import 'package:clean_architecture/domain/usecase/base_usecase.dart';
+import 'package:dartz/dartz.dart';
+
+class LoginUsecase implements BaseUsecase<LoginUsecaseInput, Authentication> {
+  final Repository _repository;
+  LoginUsecase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      LoginUsecaseInput input) async {
+    return await _repository.login(LoginRequest(
+      email: input.email,
+      password: input.password,
+      imei: 'imei',
+      deviceType: 'deviceType',
+    ));
+  }
+}
+
+class LoginUsecaseInput {
+  String email;
+  String password;
+
+  LoginUsecaseInput(this.email, this.password);
+}
