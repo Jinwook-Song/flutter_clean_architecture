579c1b754a51552243d23a2db56976a86cbf5d0f
jinwook song <wlsdnr129@naver.com>
2024-10-04 16:33:57 +0900

    data(network): dio logger

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -2,6 +2,8 @@
 
 import 'package:clean_architecture/app/constant.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 const String APPLICATION_JSON = "application/json";
 const String CONTENT_TYPE = "content-type";
@@ -27,6 +29,16 @@ class DioFactory {
       receiveTimeout: timeOut,
       headers: headers,
     );
+
+    if (kDebugMode) {
+      dio.interceptors.add(
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+        ),
+      );
+    }
 
     return dio;
   }
