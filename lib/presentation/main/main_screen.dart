ad148cead6801fdbf120f2eb66474b94cd77f9ce
jinwook song <wlsdnr129@naver.com>
2024-10-19 19:35:03 +0900

    refactor(main): with screen model

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -7,6 +7,14 @@ import 'package:clean_architecture/presentation/resources/strings_manager.dart';
 import 'package:clean_architecture/presentation/resources/value_manager.dart';
 import 'package:flutter/material.dart';
 
+class ScreenModel {
+  final String title;
+  final IconData icon;
+  final Widget screen;
+
+  const ScreenModel(this.title, this.icon, this.screen);
+}
+
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -15,20 +23,15 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final List<Widget> _pages = [
-    const HomeScreen(),
-    const SearchScreen(),
-    const NotificationsScreen(),
-    const SettingsScreen()
-  ];
-  final List<String> _titles = [
-    AppStrings.home,
-    AppStrings.search,
-    AppStrings.notifications,
-    AppStrings.settings,
+  final List<ScreenModel> _pages = [
+    const ScreenModel(AppStrings.home, Icons.home, HomeScreen()),
+    const ScreenModel(AppStrings.search, Icons.search, SearchScreen()),
+    const ScreenModel(
+        AppStrings.notifications, Icons.notifications, NotificationsScreen()),
+    const ScreenModel(AppStrings.settings, Icons.settings, SettingsScreen()),
   ];
 
-  String _title = AppStrings.home;
+  final String _title = AppStrings.home;
   int _currentIndex = 0;
 
   @override
@@ -40,7 +43,7 @@ class _MainScreenState extends State<MainScreen> {
           style: Theme.of(context).textTheme.displayMedium,
         ),
       ),
-      body: _pages[_currentIndex],
+      body: _pages[_currentIndex].screen,
       bottomNavigationBar: Container(
         decoration: const BoxDecoration(
           boxShadow: [
@@ -57,14 +60,11 @@ class _MainScreenState extends State<MainScreen> {
           currentIndex: _currentIndex,
           showSelectedLabels: false,
           items: [
-            BottomNavigationBarItem(
-                icon: const Icon(Icons.home), label: _titles[0]),
-            BottomNavigationBarItem(
-                icon: const Icon(Icons.search), label: _titles[1]),
-            BottomNavigationBarItem(
-                icon: const Icon(Icons.notifications), label: _title[2]),
-            BottomNavigationBarItem(
-                icon: const Icon(Icons.settings), label: _titles[3]),
+            for (final i in [0, 1, 2, 3])
+              BottomNavigationBarItem(
+                icon: Icon(_pages[i].icon),
+                label: _pages[i].title,
+              ),
           ],
         ),
       ),
@@ -74,7 +74,6 @@ class _MainScreenState extends State<MainScreen> {
   void _onNavTap(int index) {
     setState(() {
       _currentIndex = index;
-      _title = _titles[index];
     });
   }
 }
