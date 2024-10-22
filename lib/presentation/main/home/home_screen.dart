6d54e5cbb3e8f23dac3837b67008aa5af1507f88
jinwook song <wlsdnr129@naver.com>
2024-10-23 08:51:44 +0900

    feat(bind): view & view model

--- lib/presentation/main/home/home_screen.dart
+++ lib/presentation/main/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:clean_architecture/app/di.dart';
+import 'package:clean_architecture/presentation/main/home/home_view_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final HomeViewModel _homeViewModel = instance.get<HomeViewModel>();
+
+  @override
+  void initState() {
+    super.initState();
+    _bind();
+  }
+
+  void _bind() {
+    _homeViewModel.start();
+  }
+
+  @override
+  void dispose() {
+    _homeViewModel.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center();
