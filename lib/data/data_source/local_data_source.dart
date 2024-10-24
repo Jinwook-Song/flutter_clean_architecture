f3cf9675e5fda6f982a74cdb9c4a7ea522c9994c
jinwook song <wlsdnr129@naver.com>
2024-10-25 08:48:55 +0900

    data(cache): retrieve cached data

--- lib/data/data_source/local_data_source.dart
+++ lib/data/data_source/local_data_source.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:clean_architecture/data/network/error_handler.dart';
 import 'package:clean_architecture/data/reponses/responses.dart';
 
 const CACHE_HOME_KEY = 'CACHE_HOME_KEY';
+const CACHE_HOME_INTERVAL = 60 * 1000; // 60 seconds
 
 abstract class LocalDataSource {
   Future<HomeResponse> getHome();
@@ -12,9 +16,14 @@ class LocalDataSourceImple implements LocalDataSource {
   // run time cache
   Map<String, CachedItem> cacheMap = {};
   @override
-  Future<HomeResponse> getHome() {
-    // TODO: implement getHome
-    throw UnimplementedError();
+  Future<HomeResponse> getHome() async {
+    CachedItem? cachedItem = cacheMap[CACHE_HOME_KEY];
+
+    if (cachedItem != null && cachedItem.isValid(CACHE_HOME_INTERVAL)) {
+      return cachedItem.data;
+    } else {
+      throw ErrorHandler.handle(DataSource.CACHE_ERROR);
+    }
   }
 
   @override
@@ -29,4 +38,11 @@ class CachedItem {
   final dynamic data;
 
   CachedItem(this.data);
+}
+
+extension CachedItemX on CachedItem {
+  bool isValid(int expirationTime) {
+    int current = DateTime.now().millisecondsSinceEpoch;
+    return current - cacheTime <= expirationTime;
+  }
 }
