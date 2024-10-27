4bdc278cb25cc7ff2dbae97a8364d879b49368ee
jinwook song <wlsdnr129@naver.com>
2024-10-27 14:24:48 +0900

    refactor(cache): generic type

--- lib/data/data_source/local_data_source.dart
+++ lib/data/data_source/local_data_source.dart
@@ -1,23 +1,23 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:clean_architecture/data/network/error_handler.dart';
-import 'package:clean_architecture/data/reponses/responses.dart';
 
 const CACHE_HOME_KEY = 'CACHE_HOME_KEY';
 const CACHE_HOME_INTERVAL = 60 * 1000; // 60 seconds
 
-abstract class LocalDataSource {
-  Future<HomeResponse> getHome();
+abstract class LocalDataSource<T> {
+  Future<T> getItem(String key);
 
-  Future<void> saveHomeToCache(HomeResponse response);
+  Future<void> saveItemToCache(String key, T item);
 }
 
-class LocalDataSourceImple implements LocalDataSource {
-  // run time cache
-  Map<String, CachedItem> cacheMap = {};
+class LocalDataSourceImpl<T> implements LocalDataSource<T> {
+  // 런타임 캐시
+  Map<String, CachedItem<T>> cacheMap = {};
+
   @override
-  Future<HomeResponse> getHome() async {
-    CachedItem? cachedItem = cacheMap[CACHE_HOME_KEY];
+  Future<T> getItem(String key) async {
+    CachedItem<T>? cachedItem = cacheMap[key];
 
     if (cachedItem != null && cachedItem.isValid(CACHE_HOME_INTERVAL)) {
       return cachedItem.data;
@@ -27,20 +27,20 @@ class LocalDataSourceImple implements LocalDataSource {
   }
 
   @override
-  Future<void> saveHomeToCache(HomeResponse response) async {
-    cacheMap[CACHE_HOME_KEY] = CachedItem(response);
+  Future<void> saveItemToCache(String key, T item) async {
+    cacheMap[key] = CachedItem<T>(item);
   }
 }
 
-class CachedItem {
+class CachedItem<T> {
   int cacheTime = DateTime.now().millisecondsSinceEpoch;
 
-  final dynamic data;
+  final T data;
 
   CachedItem(this.data);
 }
 
-extension CachedItemX on CachedItem {
+extension CachedItemX<T> on CachedItem<T> {
   bool isValid(int expirationTime) {
     int current = DateTime.now().millisecondsSinceEpoch;
     return current - cacheTime <= expirationTime;
